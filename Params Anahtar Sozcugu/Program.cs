b9f5e7833b6aae7dd960afd20bc3aa309b443ae2
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-10 23:30:07 +0300

    params kullanımı parametre aktarımı, fonsiyonda kullanılır tek boyutlu dizi olur fazlası değil tanımladan önce başka değişkenlerde foksiyona eklene bilir.

--- Params Anahtar Sozcugu/Program.cs
+++ Params Anahtar Sozcugu/Program.cs
@@ -6,7 +6,38 @@ namespace Params_Anahtar_Sozcugu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             params parametler demek bir fonksiyonda kullanılır
+            fonksiyona çok fazla değer gönderilecekse kullanılır
+             */
+            Console.WriteLine(islem('+', 1, 2, 3));
+            Console.WriteLine(islem('*', 2, 2, 3));
+
+
+        }
+        static int islem(char isx, params int[] sayilar)
+        {
+            int sonuc = 0;
+            if (isx == '+')
+            {
+                Console.WriteLine("Toplama");
+                foreach (var item in sayilar)
+                {
+                    sonuc += item;
+                }
+            }
+            
+            if (isx == '*')
+            {
+               sonuc = 1;
+             Console.WriteLine("Çarp");
+                foreach (var item in sayilar)
+                {
+                    sonuc = sonuc * item;
+                }
+
+            }
+            return sonuc;
         }
     }
 }
