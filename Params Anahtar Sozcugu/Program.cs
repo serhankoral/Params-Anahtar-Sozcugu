744a549870db1a9ea8485901b080e29fc49c5619
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-10 23:11:18 +0300

    Proje dosyası ekle.

---
+++ Params Anahtar Sozcugu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Params_Anahtar_Sozcugu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
